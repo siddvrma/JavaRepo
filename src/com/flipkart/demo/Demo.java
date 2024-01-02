1d097b84f1e69b7e2aa1efd29a1926cf72398838
siddvrma <siddhanth.verma@flipkart.com>
2024-01-02 12:28:13 +0530

    Java Project

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Program");
+
+	}
+
+}
